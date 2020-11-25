b838e068822cfeedb87ec136b30ebe51715d47a5
thenamesalc <ganesh-kumar@live.com>
2020-11-25 20:42:07 +0530

    removed goaddy script that was not inmemory

--- godaddy_collector.py
+++ godaddy_collector.py
@@ -1,8 +1,14 @@
+from pathlib import Path
 import sqlite3
-from json import load
 from ftplib import FTP
 from tempfile import TemporaryFile
 from zipfile import ZipFile
+from io import BytesIO
+from json import loads
+
+
+Path('db').mkdir(exist_ok=True)
+
 
 limiter=200 # set this to None for writing all rows to db
 
@@ -20,31 +26,31 @@ date_added timestamp default CURRENT_DATE)'''
 conn=sqlite3.connect('db/godaddy_db.db',detect_types=sqlite3.PARSE_DECLTYPES)
 cur=conn.cursor()
 
+
 def download_and_extract(file_name):
 	print(f'Downloading {file_name}')
-	with TemporaryFile() as tempfile:
+	with BytesIO() as tempfile:
 		ftp.retrbinary(f'RETR {file_name}',tempfile.write)
 		with ZipFile(tempfile) as zip_:
 			file_to_extract=file_name.rsplit('.',1)[0]
-			zip_.extract(file_to_extract)
-	add_to_db(file_name)
+			with zip_.open(file_to_extract) as json_contents:
+				file_to_extract=file_name.rsplit('.',1)[0]
+				add_to_db(file_to_extract,json_contents.read().decode('utf-8'))
 
-def add_to_db(file_name):
+def add_to_db(file_name,json_contents):
 	print(f'Adding {file_name}')
-	file_name=file_name.rsplit('.',1)[0]
-	with open(file_name,encoding='utf-8') as zip_: 
-		data=[(x['domainName'],x['auctionEndTime']) for x in load(zip_)['data']][:limiter] 
-
+	keys_needed={'domainName','auctionEndTime'}
+	data=[[v if k!='auctionEndTime' else v.replace('Z','') for k,v in x.items() if k in keys_needed] for x in loads(json_contents)['data']][:limiter]
 	table_name=f'godaddy_{file_name.split(".")[0]}'
 	cur.execute(create_query.format(table_name))
-	cur.executemany(f'insert into {table_name} (domain_name,available) values (?,?)',data)
+	cur.executemany(f'insert into {table_name} (domain_name, available, domain_length) values (?,?,?)', (i+[len(i[0].split('.')[0])] for i in data)) 
 	conn.commit()
+
+
 
 with FTP('ftp.godaddy.com','auctions') as ftp:
 	files_needed=(x for x in ftp.nlst() if x.endswith('json.zip'))
-	
 	for file_ in files_needed: 
-		print(f'{file_=}')
 		download_and_extract(file_)
 
 conn.close()
