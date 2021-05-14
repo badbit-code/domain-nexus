997cbc530ff381446e2bea3bf7aab673416d7343
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-05-14 00:27:43 +0000

    Update reinitdb.sh

--- scripts/reinitdb.sh
+++ scripts/reinitdb.sh
@@ -38,32 +38,42 @@ fi
 DEFAULT_DB=defaultdb
 
 
-echo "Enter password for nexus_reader user: "
-read -s DEFAULT_NEXUS_R_PW
-echo "Enter password for nexus_collector user: "
-read -s DEFAULT_NEXUS_W_PW
 
-base_connection_stub="psql -U doadmin -h $POSTGRES_HOST -p $POSTGRES_PORT -d $DEFAULT_DB --set=sslmode=require -a" 
-nexus_connection_stub="psql -U doadmin -h $POSTGRES_HOST -p $POSTGRES_PORT -d nexus --set=sslmode=require -a" 
+
+base_connection_stub="psql -U doadmin -h $POSTGRES_HOST -p $POSTGRES_PORT -d $DEFAULT_DB --set=sslmode=require" 
+nexus_connection_stub="psql -U doadmin -h $POSTGRES_HOST -p $POSTGRES_PORT -d nexus --set=sslmode=require" 
 
 # Destroy nexus db and users
+$nexus_connection_stub -c "REVOKE ALL ON ALL TABLES IN SCHEMA public FROM nexus_collector"
+$nexus_connection_stub -c "REVOKE ALL ON ALL TABLES IN SCHEMA public FROM nexus_collector"
+$nexus_connection_stub -c "REVOKE ALL ON ALL TABLES IN SCHEMA public FROM nexus_reader"
+$nexus_connection_stub -c "REVOKE ALL ON ALL TABLES IN SCHEMA public FROM nexus_reader"
 $base_connection_stub -c "DROP DATABASE nexus"
-$base_connection_stub -c "DROP USER nexus_collector"
 $base_connection_stub -c "DROP USER nexus_reader"
+$base_connection_stub -c "DROP USER nexus_collector"
 
 # Create nexus db
 $base_connection_stub -c "CREATE DATABASE nexus"
 
+echo "Enter password for nexus_reader user: "
+read -s DEFAULT_NEXUS_R_PW
+echo "Enter password for nexus_collector user: "
+read -s DEFAULT_NEXUS_W_PW
 # Create nexus users
-$base_connection_stub -c "CREATE user nexus_reader ENCRYPTED PASSWORD '$DEFAULT_NEXUS_R_PW' NOCREATEDB VALID UNTIL 'infinity'" 
-$base_connection_stub -c "CREATE user nexus_collector ENCRYPTED PASSWORD '$DEFAULT_NEXUS_W_PW' NOCREATEDB VALID UNTIL 'infinity'"
-
-# Grant priviliges 
-$base_connection_stub -c "GRANT CONNECT ON DATABASE nexus to nexus_collector"
-$base_connection_stub -c "GRANT ALL PRIVILEGES ON DATABASE nexus to nexus_collector"
-
-$base_connection_stub -c "GRANT CONNECT ON DATABASE nexus to nexus_reader"
-$base_connection_stub -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO nexus_reader"
+$base_connection_stub -c "CREATE user nexus_reader ENCRYPTED PASSWORD '$DEFAULT_NEXUS_R_PW' VALID UNTIL 'infinity'" 
+$base_connection_stub -c "CREATE user nexus_collector ENCRYPTED PASSWORD '$DEFAULT_NEXUS_W_PW' VALID UNTIL 'infinity'"
 
 # Initialize the nexus DB
 $nexus_connection_stub -f ./src/container/postgresql/init.sql
+
+# Grant priviliges 
+$nexus_connection_stub -c "GRANT all PRIVILEGES ON DATABASE nexus to nexus_collector"
+$nexus_connection_stub -c "GRANT ALL ON SCHEMA public TO nexus_collector"
+$nexus_connection_stub -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO nexus_collector"
+$nexus_connection_stub -c "GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO nexus_collector"
+$nexus_connection_stub -c "GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO nexus_collector"
+
+$nexus_connection_stub -c "GRANT all PRIVILEGES ON DATABASE nexus to nexus_reader"
+$nexus_connection_stub -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO nexus_reader"
+
+
